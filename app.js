4084002317dcc9f75bf06ed75819a18f4a1da847
Aylaataydir <aylaataydir@gmail.com>
2025-11-16 11:05:48 +0100

    add classname for icons

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const slider = document.querySelector(".slider");
+const sliderWrapper = document.querySelector(".slider-wrapper");
+
+
+let currentlyTransport = 0;
+const scrollWidth = slider.scrollWidth
+const clientWidth = sliderWrapper.clientWidth
+const maxScroll = scrollWidth - clientWidth
+const Transport = 500px;
+
+document.addEventListener("click", () => {
+
+
+
+
+})
