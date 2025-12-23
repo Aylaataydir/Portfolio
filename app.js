cf1c9ac01cd043f09c84146d824219f551a9b4a5
Aylaataydir <aylaataydir@gmail.com>
2025-12-23 21:38:53 +0100

    ...

--- app.js
+++ app.js
@@ -1,17 +1,68 @@
 
 const slider = document.querySelector(".slider");
-const sliderWrapper = document.querySelector(".slider-wrapper");
+const sliderContainer = document.querySelector(".slider-container");
+const arrowRight = document.querySelector(".icon-right");
+const arrowLeft = document.querySelector(".icon-left")
 
 
 let currentlyTransport = 0;
 const scrollWidth = slider.scrollWidth
-const clientWidth = sliderWrapper.clientWidth
+const clientWidth = sliderContainer.clientWidth
 const maxScroll = scrollWidth - clientWidth
-const Transport = 500px;
+const transport = 500;
 
-document.addEventListener("click", () => {
+const leftGap = (clientWidth-scrollWidth) / 2
+
+// arrowRight.addEventListener("click", () => {
+
+// if(currentlyTransport < maxScroll) {
+//     currentlyTransport -= transport
+// }
+
+// if(-currentlyTransport > maxScroll) {
+//     currentlyTransport += 
+// }
+
+//      slider.style.transform = `translateX(${currentlyTransport}px)`
+
+// })
+
+// arrowLeft.addEventListener("click", () => {
+
+// currentlyTransport -= transport
+
+
+// slider.style.transform = `translateX(${currentlyTransport}px)`
+// })
+
+arrowRight.addEventListener("click", () => {
+    if (currentlyTransport < maxScroll) {
+        currentlyTransport += transport;
+
+        if (currentlyTransport > maxScroll) {
+            currentlyTransport = maxScroll;
+        }
+
+        slider.style.transform = `translateX(-${currentlyTransport}px)`;
+    }
+});
+
+arrowLeft.addEventListener("click", () => {
+    if (currentlyTransport > 0) {
+        currentlyTransport -= transport;
+
+        if (currentlyTransport < 0) {
+            currentlyTransport = 0;
+        }
+
+        slider.style.transform = `translateX(-${currentlyTransport}px)`;
+    }
+});
 
 
 
+"use strict";
+ isim = 0;   
 
-})
+
+
